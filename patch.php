0a3904fe86cc4d4145b01e5afd50d324cd1f1ce5
Muntasir Mohiuddin <muntasir.mohiuddin@gmail.com>
2013-05-01 15:56:46 -0400

    added class and method patching. more to follow.

--- patch.php
+++ patch.php
@@ -1,77 +1,131 @@
 <?php
+/**
+* Copyright (c) 2013 Muntasir Mohiuddin<muntasir.mohiuddin@gmail.com>
+* 
+* Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+* 
+* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+* 
+* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+*/
 namespace MonkeyPatch;
 
 class Patch {
+
 	static $_default	= array(
-		'method' => array(
-				'visbility' => 'public',
-				'code' => 'return True;',
-			),
 		'property' => array(
+				'reference' => NULL,
 				'visbility' => 'public',
 				'value' => 'True',
 			),
+
+		'method' => array(
+				'reference' => NULL,
+				'visbility' => 'public',
+				'code' => 'return True;',
+			),
+
 	);
 
 	/**
-	* Apply a patch from the patches folder
+	* Remove the namespace from the name and return defined name
 	* 
-	* @param string $class_name name of the class that requires patching
-	* @return boolean returns True/False based on if the patch loaded successfully
+	* @param string $name name to remove namespace from
+	* @return string sanitized name without the namespace
 	*/
-	public static function apply($class_name){
-		try{
-			require_once(sprintf("patches/%s.php", $class_name));
-		} catch(Exception $e){
-			return False;
-		}
-
-		return True;
+	public static function removeNamespace($name) {
+		$name_segments = explode("\\", $name);
+		return end($name_segments);
 	}
 
 	/**
-	* Redefine a function based on the config provided.  
+	* Returns the full qualified path of the provided filename.
+	* If the file does not exist in that locauon, returns FALSE
 	* 
-	*  		
+	* @param string $file_name name of the file
+	* @return mixed False if file does not exist. Full path as string if does.
 	*/
-	public static function redefine($config=NULL){
-		// is class name provided ?
-		if (is_null($config) || empty($config['class_name'])){
+	public static function getQualifiedPath($file_name) {
+		$qualified_file = sprintf("%s/patches/%s.php", dirname(__FILE__), $file_name);
+
+		if(!file_exists($qualified_file)) {
 			return False;
-		}
+		} 
 
-		$redefined_properties_code = "";
-		foreach($config['properties'] as $property){
-			$name = $property['name'];
-			$visbility = (!empty($property['visbility'])) ? $property['visbility'] : self::$_default['property']['visbility'];
-			$value = (!empty($property['value'])) ? $property['value'] : self::$_default['property']['value'];
+		return $qualified_file;
+	}
 
-			$redefined_properties_code .= sprintf("\t%s $%s = %s;\n", $visbility, $name, $value);
-		}
-
-
-		$redefined_methods_code = "";
-		foreach($config['methods'] as $method){
-			$name = $method['name'];
-			
-			$visbility = (!empty($method['visbility'])) ? $method['visbility'] : self::$_default['method']['visbility'];
-			
-
-			// if method args passed
-			$args = "";
-			if(!empty($method['args'])){
-				$args = sprintf("%s", implode(",", $method['args']));
-			}
-
-			$code = (!empty($method['code'])) ? $method['code'] : self::$_default['method']['code'];
-
-			$redefined_methods_code .= sprintf("\t%s function %s(%s){\n%s\n}", $visbility, $name, $args, $code);
-		}
-		$code = sprintf("class %s {\n%s\n\n%s\n}", $config['class_name'], $redefined_properties_code, $redefined_methods_code);
+	/**
+	* Patch a class.
+	* 
+	* @param $string $class_name Classname which is getting patched.
+	* @return boolean TRUE on success or FALSE on failure	
+	*/
+	static function cls($class_name) {
+		# full path to the patch class
+		$qualified_file = self::getQualifiedPath(
+			self::removeNamespace($class_name)  # find the name of the class by removing namespace(s)
+		);
 		
-		eval($code);
+		if($qualified_file) { // there is a file by that name
+			# import classe and force override - mainly for namespaced classes
+			return runkit_import($qualified_file, RUNKIT_IMPORT_CLASSES|RUNKIT_IMPORT_OVERRIDE);
+		}
+
+		return False;  # by default return False
+	}  
+
+	/**
+	* Patch a method of a give class.
+	* 
+	* @param $string $class_name Classname of the method including namespace.
+	* @param $string $method_name Method to patch.
+	* @param $string $code Custom code to patch the method with. If no code is passed 
+	*						the system will try to load code from 
+	*						patches/<class name>.<method name>.php file. If that file is 
+	*						not present or has no content applies default method code.
+	* @return boolean TRUE on success or FALSE on failure	
+	*/
+	static function method($class_name, $method_name, $code=NULL){
+		$class = new \ReflectionClass($class_name);
+
+		$method = $class->getMethod($method_name);
+
+		$visbility = RUNKIT_ACC_PUBLIC;  # usually public methods get changed
+
+		if($method->isPrivate()){  # if method is private
+			$visbility = RUNKIT_ACC_PRIVATE;
+		} 
+
+		if($method->isStatic()) {  # if the method is of static reference
+			$visbility = $visbility|RUNKIT_ACC_STATIC;
+		}
 		
-	} // end of redefine 
+		$parameters = array();  # save parameters here
+
+		foreach($method->getParameters() as $parameter){
+			$parameters[] = "$" . $parameter->name;
+		}
+
+		# no code passed, check if there are any flle in patches
+		if(empty($code)){
+			# the file name is required to be <class_name>.<method name>
+			$sanitized_class_name = self::removeNamespace($class_name);
+			$qualified_file = self::getQualifiedPath($sanitized_class_name . "." . $method_name);
+			
+			if($qualified_file) {  # if file exists
+				$code = file_get_contents($qualified_file);
+			} 
+		}
+		
+		return runkit_method_redefine(
+			$class_name,  # class name
+			$method_name,  # method name 
+			implode(",", $parameters),  # paramters
+			# if code is either empty ot not set(NULL), assing default code
+			(!empty($code)) ? $code : self::$_default['method']['code'],  # custom code
+			$visbility  # visibility
+		);
+	}
 }
-
 ?>
